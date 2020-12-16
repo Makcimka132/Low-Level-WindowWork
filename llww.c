b42e3dea47eacc67f175e95f646abfbfc570fc9b
Makcimka132 <60743464+Makcimka132@users.noreply.github.com>
2020-12-17 00:16:54 +0300

    Update llww.c

--- llww.c
+++ llww.c
@@ -6,14 +6,10 @@ void msgbox(char *name, char *str){MessageBox(NULL,name,str,MB_OK);}
 
 void HideConsole()
 {
-
     HWND hwnd = GetConsoleWindow();ShowWindow(hwnd,0);
-
 }
 
 void ShowConsole()
 {
-
     HWND hwnd = GetConsoleWindow();ShowWindow(hwnd,1);
-
 }

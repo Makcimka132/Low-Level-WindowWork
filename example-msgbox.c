90662efeae1462b1c0f43e9dba5c4d70fd6a70dc
Makcimka132 <60743464+Makcimka132@users.noreply.github.com>
2020-12-16 23:49:30 +0300

    Delete example-msgbox.c

--- example-msgbox.c
+++
@@ -1,6 +0,0 @@
-#include "llww.h" // - Include a library
-int main(){
-    HideConsole(); // - Hide a console
-    msgbox("Test name", "Test str"); // - Messagebox
-    return 0; // - Stop a program
-}

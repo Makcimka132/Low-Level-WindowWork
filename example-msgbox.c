88b4ab97ea665a2f6081f2b256e07aed2d10a8b7
Makcimka132 <60743464+Makcimka132@users.noreply.github.com>
2020-12-16 23:47:10 +0300

    Create example-msgbox.c

---
+++ example-msgbox.c
@@ -0,0 +1,6 @@
+#include "llww.h" // - Include a library
+int main(){
+    HideConsole(); // - Hide a console
+    msgbox("Test name", "Test str"); // - Messagebox
+    return 0; // - Stop a program
+}

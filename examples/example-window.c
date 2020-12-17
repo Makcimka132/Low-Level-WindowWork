6d11d36ed02c40ef117fb2df90ca637d01fa3ba1
Makcimka132 <60743464+Makcimka132@users.noreply.github.com>
2020-12-17 11:50:19 +0300

    Update example-window.c

--- examples/example-window.c
+++ examples/example-window.c
@@ -1,16 +1,16 @@
-#include "llww.h"
+#include "llww.h" // Include a library
 
 int main()
 {
-	DestroyConsole();
-	InitWindow(10, 10, 640, 480, "Test");
+	DestroyConsole(); // Destroy a console
+	InitWindow(10, 10, 640, 480, "Test"); // Initialization a window
 	MSG msg;
-	while (GetMessage(&msg, NULL, 0, 0))
+	while (GetMessage(&msg, NULL, 0, 0)) // if get a message...
 	{
 		DispatchMessage(&msg);
-		if ((msg.message == 161) && (msg.wParam == 20))
+		if ((msg.message == 161) && (msg.wParam == 20)) // if press "Close"...
 		{
-			return 0;
+			return 0; // Close a programm
 		}
 	}
     return 0;
